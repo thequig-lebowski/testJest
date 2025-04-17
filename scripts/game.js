e08b3a750d4f00cf63f4f08e11ec32a1c9278d9f
David O'Quigley <oquigld2@gmail.com>
2025-04-17 13:01:01 +0000

    Add stuff

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,7 @@ let game = {
     score: 0,
     currentGame: [],
     playerMoves: [],
+    turnNumber: 0,
     choices: ['button1', 'button2', 'button3', 'button4'],
 };
 
@@ -17,7 +18,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
 function showScore() {
@@ -31,4 +32,15 @@ function lightsOn(circ) {
     }, 400)
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber ++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -27,6 +28,9 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains correct ids", () => {
         expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4']);
+    });
+    test('turnNumber key exists', () => {
+        expect('turnNumber' in game).toBe(true);
     });
 });
 
@@ -72,5 +76,10 @@ describe('gameplay works correctly', () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain('light');
+    });
+    test('showTurns should update game.turnNumber', () => {
+        game.turnNumnber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
