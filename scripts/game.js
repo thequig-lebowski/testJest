ed15e8870eafa6007e9a0761f26db7b3e6099e52
David O'Quigley <oquigld2@gmail.com>
2025-04-17 11:36:26 +0000

    Add test and functions

---
+++ scripts/game.js
@@ -0,0 +1,34 @@
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ['button1', 'button2', 'button3', 'button4'],
+};
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+};
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+    document.getElementById('score').innerText = game.score;
+};
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add('light');
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove('light');
+    }, 400)
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
